0a0ec3239b1a3a9021c607f116626db9b331798a
Horman69 <horman69@users.noreply.github.com>
2026-01-15 19:07:58 +0700

    Упрощен раздел Образование - только ООП НОО и ООО, убрана статистика

--- app/sveden/education/page.tsx
+++ app/sveden/education/page.tsx
@@ -57,22 +57,6 @@ export default function EducationPage() {
                                     </div>
                                 </div>
 
-                                {/* ФГОС НОО */}
-                                <div>
-                                    <h3 className="font-semibold mb-2">Федеральные государственные образовательные стандарты:</h3>
-                                    <div className="space-y-2">
-                                        <DocumentLink href="/documents/noo/prikaz-286-fgos-noo.pdf" itemprop="eduFedDoc">
-                                            Приказ №286 от 31.05.2021 ФГОС НОО
-                                        </DocumentLink>
-                                        <DocumentLink href="/documents/noo/prikaz-569-fgos-noo.pdf" itemprop="eduFedDoc">
-                                            Приказ №569 от 18.07.2022 об изменении ФГОС НОО
-                                        </DocumentLink>
-                                        <DocumentLink href="https://cloud.mail.ru/public/iS67/zV4vdMxjQ" itemprop="eduFedDoc">
-                                            Приказ №704 от 09.10.2024 о внесении изменений в ФОП
-                                        </DocumentLink>
-                                    </div>
-                                </div>
-
                                 {/* ООП НОО */}
                                 <div>
                                     <h3 className="font-semibold mb-2">Основная образовательная программа:</h3>
@@ -80,40 +64,6 @@ export default function EducationPage() {
                                         Основная образовательная программа НОО
                                     </DocumentLink>
                                 </div>
-
-                                {/* Учебный план */}
-                                <div>
-                                    <h3 className="font-semibold mb-2">Учебный план:</h3>
-                                    <DocumentLink href="/documents/noo/Учебный-план-НОО.docx" itemprop="educationPlan">
-                                        Учебный план НОО
-                                    </DocumentLink>
-                                </div>
-
-                                {/* Календарный график */}
-                                <div>
-                                    <h3 className="font-semibold mb-2">Календарный учебный график:</h3>
-                                    <DocumentLink href="/documents/noo/kalendarnyj-uchebnyj-grafik-noo.pdf" itemprop="educationShedule">
-                                        Календарный учебный график НОО
-                                    </DocumentLink>
-                                </div>
-
-                                {/* Рабочие программы */}
-                                <div>
-                                    <h3 className="font-semibold mb-2">Рабочие программы дисциплин:</h3>
-                                    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-                                        <DocumentLink href="/documents/noo/русский-язык.pdf" itemprop="educationRpd">Русский язык</DocumentLink>
-                                        <DocumentLink href="/documents/noo/литературное-чтение.pdf" itemprop="educationRpd">Литературное чтение</DocumentLink>
-                                        <DocumentLink href="/documents/noo/английский-язык.pdf" itemprop="educationRpd">Английский язык</DocumentLink>
-                                        <DocumentLink href="/documents/noo/математика.pdf" itemprop="educationRpd">Математика</DocumentLink>
-                                        <DocumentLink href="/documents/noo/окружающий-мир.pdf" itemprop="educationRpd">Окружающий мир</DocumentLink>
-                                        <DocumentLink href="/documents/noo/ОРКСЭ.pdf" itemprop="educationRpd">ОРКСЭ</DocumentLink>
-                                        <DocumentLink href="/documents/noo/ИЗО.pdf" itemprop="educationRpd">Изобразительное искусство</DocumentLink>
-                                        <DocumentLink href="/documents/noo/музыка.pdf" itemprop="educationRpd">Музыка</DocumentLink>
-                                        <DocumentLink href="/documents/noo/труд.pdf" itemprop="educationRpd">Технология</DocumentLink>
-                                        <DocumentLink href="/documents/noo/Физ-кул.pdf" itemprop="educationRpd">Физическая культура</DocumentLink>
-                                    </div>
-                                </div>
-
 
                             </div>
                         </CardContent>
@@ -142,22 +92,6 @@ export default function EducationPage() {
                                     </div>
                                 </div>
 
-                                {/* ФГОС ООО */}
-                                <div>
-                                    <h3 className="font-semibold mb-2">Федеральные государственные образовательные стандарты:</h3>
-                                    <div className="space-y-2">
-                                        <DocumentLink href="/documents/ooo/prikaz-287-fgos-ooo.pdf" itemprop="eduFedDoc">
-                                            Приказ №287 от 31.05.2021 ФГОС ООО
-                                        </DocumentLink>
-                                        <DocumentLink href="/documents/ooo/prikaz-568-fgos-ooo.pdf" itemprop="eduFedDoc">
-                                            Приказ №568 от 18.07.2022 о изменении ФГОС ООО
-                                        </DocumentLink>
-                                        <DocumentLink href="https://cloud.mail.ru/public/iS67/zV4vdMxjQ" itemprop="eduFedDoc">
-                                            Приказ №704 от 09.10.2024 о внесении изменений в ФОП
-                                        </DocumentLink>
-                                    </div>
-                                </div>
-
                                 {/* ООП ООО */}
                                 <div>
                                     <h3 className="font-semibold mb-2">Основная образовательная программа:</h3>
@@ -165,43 +99,6 @@ export default function EducationPage() {
                                         Основная образовательная программа ООО
                                     </DocumentLink>
                                 </div>
-
-                                {/* Учебный план */}
-                                <div>
-                                    <h3 className="font-semibold mb-2">Учебный план:</h3>
-                                    <DocumentLink href="/documents/ooo/Учебный-план-ООО.docx" itemprop="educationPlan">
-                                        Учебный план ООО
-                                    </DocumentLink>
-                                </div>
-
-                                {/* Календарный график */}
-                                <div>
-                                    <h3 className="font-semibold mb-2">Календарный учебный график:</h3>
-                                    <DocumentLink href="/documents/ooo/Календарный-учебный-график-ООО.docx" itemprop="educationShedule">
-                                        Календарный учебный график ООО
-                                    </DocumentLink>
-                                </div>
-
-                                {/* Рабочие программы */}
-                                <div>
-                                    <h3 className="font-semibold mb-2">Рабочие программы дисциплин:</h3>
-                                    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-                                        <DocumentLink href="/documents/ooo/Русский-язык.pdf" itemprop="educationRpd">Русский язык</DocumentLink>
-                                        <DocumentLink href="/documents/ooo/Литература.pdf" itemprop="educationRpd">Литература</DocumentLink>
-                                        <DocumentLink href="/documents/ooo/Английский-язык.pdf" itemprop="educationRpd">Английский язык</DocumentLink>
-                                        <DocumentLink href="/documents/ooo/Математика.pdf" itemprop="educationRpd">Математика</DocumentLink>
-                                        <DocumentLink href="/documents/ooo/Информатика.pdf" itemprop="educationRpd">Информатика</DocumentLink>
-                                        <DocumentLink href="/documents/ooo/История.pdf" itemprop="educationRpd">История</DocumentLink>
-                                        <DocumentLink href="/documents/ooo/География.pdf" itemprop="educationRpd">География</DocumentLink>
-                                        <DocumentLink href="/documents/ooo/Биология.pdf" itemprop="educationRpd">Биология</DocumentLink>
-                                        <DocumentLink href="/documents/ooo/Физика.pdf" itemprop="educationRpd">Физика</DocumentLink>
-                                        <DocumentLink href="/documents/ooo/ИЗО.pdf" itemprop="educationRpd">Изобразительное искусство</DocumentLink>
-                                        <DocumentLink href="/documents/ooo/Музыка.pdf" itemprop="educationRpd">Музыка</DocumentLink>
-                                        <DocumentLink href="/documents/ooo/Труд.pdf" itemprop="educationRpd">Технология</DocumentLink>
-                                        <DocumentLink href="/documents/ooo/Физическая-культура.pdf" itemprop="educationRpd">Физическая культура</DocumentLink>
-                                    </div>
-                                </div>
-
 
                             </div>
                         </CardContent>
